92aab22dfc1a2b6a704dd16aab9e0427aaf3293a
RiyaBasak26 <riyabasak26@gmail.com>
2024-04-14 01:15:09 +0530

    added design in index.css for input field

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ function App() {
   return (
     <>
       <Button label='Register' type='button' />
+      <label> checkbox</label> 
+      <input  type='checkbox'/>
+      <input type='radio'/>
+      <input type='text' placeholder='name'/>
+      <select name="cars" id="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select> 
+
+
     </>
   )
 }
