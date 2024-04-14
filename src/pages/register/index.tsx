81afe47d46f1fd4731c4c5f16806b8de4e4100c8
dipstech1 <panja.souradeep@gmail.com>
2024-04-14 12:56:55 +0530

    register page layout test

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,6 +1,24 @@
 
 export const Register = () => {
   return (
-    <h1>Register</h1>
+    <div>
+      <h1>Register</h1>
+      <div>
+        <label htmlFor="username">User Name</label>
+        <input placeholder="name" id="username"/>
+      </div>
+      <div>
+        <label htmlFor="Email">Email</label>
+        <input placeholder="email" id="Email" />
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input placeholder="password" id="password" type="password" />
+      </div>
+
+      <div>
+        <button disabled>Submit</button>
+      </div>
+    </div>
   )
 }
--- src/pages/register/tests/register.spec.tsx
+++ src/pages/register/tests/register.spec.tsx
@@ -1,12 +1,54 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 
-import {Register} from '../../register';
+import { Register } from '../../register';
 
-describe("Register screen", ()=>{
+describe("Register screen", () => {
 
-    it("should render header", ()=>{
-        render(<Register />);
-        const header = screen.getByRole("heading", {name : "Register"});
-        expect(header).toBeDefined()
+    describe("Layout", () => {
+        it("should render header", () => {
+            render(<Register />);
+            const header = screen.getByRole("heading", { name: "Register" });
+            expect(header).toBeInTheDocument()
+        });
+
+        it("should render 2 input fields" , ()=>{
+            let registerComp = render(<Register />);
+            const inputs =  registerComp.container.querySelectorAll("input");
+            expect(inputs).toHaveLength(3)
+        });
+
+        it("should have username input label", ()=>{
+            render(<Register />);
+            const nameLabel = screen.getByLabelText("User Name");
+            expect(nameLabel).toBeInTheDocument();
+        });
+
+        it("should contain email input" , ()=>{
+            render(<Register />);
+            const emailInp = screen.getByPlaceholderText("email"); 
+            expect(emailInp).toBeInTheDocument();
+
+        });
+
+        it("should contain password field", ()=>{
+            render(<Register />);
+            const passwordInp = screen.getByPlaceholderText("password"); 
+            expect(passwordInp).toBeInTheDocument();
+        });
+
+        it("password field sould have type password", ()=>{
+            render(<Register/>);
+            const passwordInp =  screen.getByPlaceholderText("password"); 
+            expect(passwordInp).toHaveAttribute("type", "password")
+        });
+
+        it("should render a button with disable state", ()=>{
+            render(<Register/>);
+            const btn = screen.getByRole("button",{name:"Submit"});
+            expect(btn).toBeDisabled()
+        })
     })
-})
+
+
+}) 
